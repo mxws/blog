a53291aeceea4307fa46cff2a5fe9aad27e329e1
mwise34 <max.wise@gatech.edu>
2020-04-01 09:31:05 -0700

    Initial push

---
+++ blog.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Max Wise',
+        'title': 'Test Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 1, 2020'
+    },
+    {
+        'author': 'Test Guy',
+        'title': 'Test Post 2',
+        'content': 'Second post content',
+        'date_posted': 'March 1, 2020'
+    }
+]
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+if __name__ == '__main__':
+    app.run(debug=True)
